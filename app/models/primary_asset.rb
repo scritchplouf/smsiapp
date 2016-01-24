47b9d5bb1e643fda9e62365d4da87710e37962d9
ClemLass <clemlass@gmail.com>
2016-01-24 23:49:31 +0100

    ajout de la table de join

--- app/models/primary_asset.rb
+++ app/models/primary_asset.rb
@@ -1,2 +1,3 @@
 class PrimaryAsset < ActiveRecord::Base
+  has_and_belongs_to_many :secondary_assets
 end
--- app/models/secondary_asset.rb
+++ app/models/secondary_asset.rb
@@ -1,2 +1,3 @@
 class SecondaryAsset < ActiveRecord::Base
+  has_and_belongs_to_many :primary_assets
 end
